0717012b1bc093302ebbac5dd73d15bc0c4ec6db
frani <franicons@gmail.com>
2025-03-22 22:54:52 +0100

    building-profile

--- lib/globalProvider.tsx
+++ lib/globalProvider.tsx
@@ -24,7 +24,6 @@ export const GlobalProvider = ({children}: {children: ReactNode}) => {
         fn: getCurrentUser
     });
     const isLoggedIn = !! user;
-    console.log(JSON.stringify(user, null, 2))
     return (
         <GlobalContext.Provider value={{isLoggedIn, user, loading, refetch}}>
             {children}
